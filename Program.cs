134774618138027a44660acffa85a21fd0fc4781
Viney <vineykhaneja999@gmail.com>
2022-02-14 11:32:03 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Length
+{
+    internal class Length
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+
+        }
+    }
+}
+
