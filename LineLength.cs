ae2691596ebe50b45e0eb50d21d5f73b6ed2b637
Viney <vineykhaneja999@gmail.com>
2022-02-15 11:35:46 +0530

    [Viney] FEATURE ADDED: Comparing Length by Class & Objects

---
+++ LineLength.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Length_Comparison
+{
+    public class LineLength
+    {
+        int x1;
+        int x2;
+        int y1;
+        int y2;
+
+        public LineLength(int x1,int y1, int x2, int y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+        public double CalculatingLength()
+        {
+            double lengthOfLine = Math.Pow(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2), 0.5);
+            return lengthOfLine;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Line_Length_Comparison;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +11,13 @@ namespace Line_Length
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter Co-ordinates of 1st Line");
-            double line1Length = LineLength();
-            Console.WriteLine("Enter Co-ordinates of 2nd Line");
-            double line2Length = LineLength();
+            LineLength L1 = new LineLength(2, 2, 5, 6);
+            double line1Length = L1.CalculatingLength();
+            Console.WriteLine($"The length of 1st line is {line1Length}");
+            LineLength L2 = new LineLength(10, 9, 5, 6);
+            double line2Length = L2.CalculatingLength();
+            Console.WriteLine($"The length of 2nd line is {line2Length}");
             int result = line1Length.CompareTo(line2Length);
-
             switch (result)
             {
                 case 0:
@@ -28,20 +30,6 @@ namespace Line_Length
                     Console.WriteLine("Line 2 is greater than Line 1");
                     break;
             }
-        }
-        public static double LineLength()
-        {
-            //VARIABLES
-            Console.Write("Enter 1st X co-ordinate of Line: ");
-            double x11 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter 1st Y co-ordinate of Line: ");
-            double y11 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter 2nd X co-ordinate of Line: ");
-            double x12 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter 2nd Y co-ordinate of Line: ");
-            double y12 = Convert.ToDouble(Console.ReadLine());
-            double lengthOfLine = Math.Pow(Math.Pow(x12 - x11, 2) + Math.Pow(y12 - y11, 2), 0.5);
-            return lengthOfLine;
         }
     }
 }
